8410661b22484f3ea130f0e7be3e58b57d65ebca
Rhelcris Salvino de Sousa <rhelcris@gmail.com>
2020-06-27 16:32:54 -0300

    Buscando 3 imagens de gatos com chapeu e 3 fotos de gatos com óculos

--- src/main/java/br/com/itau/catapi/CatColectorApiApplication.java
+++ src/main/java/br/com/itau/catapi/CatColectorApiApplication.java
@@ -3,6 +3,7 @@ package br.com.itau.catapi;
 import br.com.itau.catapi.beans.Foto;
 import br.com.itau.catapi.beans.Gato;
 import br.com.itau.catapi.beans.Raca;
+import br.com.itau.catapi.dto.CatDTO;
 import br.com.itau.catapi.enums.TipoFoto;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
@@ -69,6 +71,42 @@ public class CatColectorApiApplication {
 			for (Gato gato : gatos) {
 				System.out.println(gato);
 			}
+
+
+
+			// Até 3 Fotos com chapéu
+			String URL_FOTOS = URL_BASE + "/images/search?limit=3&category_ids=1&order=ASC";
+			List<Gato> gatosComChapeu = new ArrayList<>();
+			ResponseEntity<List<CatDTO>> fotosComChapeuResponse = restTemplate.exchange(
+					URL_FOTOS, HttpMethod.GET, null, new ParameterizedTypeReference<List<CatDTO>>() {
+					});
+			List<CatDTO> fotosComChapeu = fotosComChapeuResponse.getBody();
+			fotosComChapeu.forEach(foto -> {
+				Foto fotoComChapeu = Foto.builder().urlFoto(foto.getUrl()).tipoFoto(TipoFoto.FOTO_CHAPEU).build();
+				Raca raca = foto.getRaca() != null && foto.getRaca().size() > 0 ? foto.getRaca().get(0) : null;
+
+				Gato gato = Gato.builder().raca(raca).fotos(Arrays.asList(fotoComChapeu)).build();
+				gatosComChapeu.add(gato);
+			});
+
+			System.out.println(">>>> GATOS COM CHAPEU: " + gatosComChapeu);
+
+
+			// Fotos com óculos
+			URL_FOTOS = URL_BASE + "/images/search?limit=3&category_ids=4&order=ASC";
+			List<Gato> gatosComOculos = new ArrayList<>();
+			ResponseEntity<List<CatDTO>> fotosComOculosResponse = restTemplate.exchange(
+					URL_FOTOS, HttpMethod.GET, null, new ParameterizedTypeReference<List<CatDTO>>() {
+					});
+			List<CatDTO> fotosDeOculos = fotosComOculosResponse.getBody();
+			fotosDeOculos.forEach(foto -> {
+				Foto fotoComOculos = Foto.builder().urlFoto(foto.getUrl()).tipoFoto(TipoFoto.FOTO_OCULOS).build();
+				Raca raca = foto.getRaca() != null && foto.getRaca().size() > 0 ? foto.getRaca().get(0) : null;
+
+				Gato gato = Gato.builder().raca(raca).fotos(Arrays.asList(fotoComOculos)).build();
+				gatosComOculos.add(gato);
+			});
+			System.out.println(">>>> GATOS COM OCULOS: " + gatosComOculos);
 
 		};
 	}
