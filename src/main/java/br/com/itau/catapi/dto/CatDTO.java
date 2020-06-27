0bcf3a9e2c931724e246259e74d4d1edda8b9942
Rhelcris Salvino de Sousa <rhelcris@gmail.com>
2020-06-27 16:23:25 -0300

    Adicionando o DTO para pegar as fotos com chapeu e oculos

---
+++ src/main/java/br/com/itau/catapi/dto/CatDTO.java
@@ -0,0 +1,19 @@
+package br.com.itau.catapi.dto;
+
+import br.com.itau.catapi.beans.Foto;
+import br.com.itau.catapi.beans.Raca;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+public class CatDTO {
+
+    @JsonAlias("breeds")
+    private List<Raca> raca;
+    @JsonAlias("url")
+    private String url;
+
+}
