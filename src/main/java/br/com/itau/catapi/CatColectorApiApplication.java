56136026b1852e74a2ccedb79c1e96e1139cd0e4
Rhelcris Salvino de Sousa <rhelcris@gmail.com>
2020-06-27 16:18:44 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/itau/catapi/CatColectorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.catapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatColectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatColectorApiApplication.class, args);
+	}
+
+}
